c5fafe1973b92d13e5dfa16778ebe6bcca5381a1
Катя <@DESKTOP-8CVJE59>
2024-10-18 22:00:07 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] array = new int[10]; 
+        Random random = new Random();
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] = random.Next(0, 21);
+        }
+
+        Console.WriteLine("Вихідний масив:");
+        for (int i = 0; i < array.Length; i++)
+        {
+            Console.Write(array[i] + " ");
+        }
+        Console.WriteLine();
+        int product = 1;  
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (array[i] >= 0 && array[i] <= 9)
+            {
+                product *= array[i];
+            }
+        }
+        Console.WriteLine("Добуток одноцифрових елементів: " + product);
+    }
+}
